918dfc56512e9205a00547cc02f5334e55aa9cb1
Eva Reitzel <evareitzel@gmail.com>
2021-10-17 10:22:59 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,62 @@
+function add() {
+  return (number +=5)
+}
+
+function subtract() {
+    return (number -+9)
+}
+
+function multiply() {
+    return (number *= 2)
+}
+
+function divide() {
+    return (number /= 5)
+}
+
+function add(a, b) {
+    return (a += b)
+    a = 2
+    b = 7
+  }
+
+  function subtract(a, b) {
+    return (a -= b)
+    a = 200
+    b = 60
+  }
+
+function multiply(a, b) {
+    return (a *= b)
+    a = 5
+    b = 12
+}
+
+function divide(a, b) {
+    return (a /= b)
+    a = 49
+    b = 7
+}
+
+function increment(n) {
+    return (++n)
+}
+
+function decrement(n) {
+    return (--n)
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+
+// parseInt("2", 10); //=> 2
+/*
+parseInt("2.2222", 10)
+
+parseInt("bhiph8794poi6wt3566t", 10);
+*/
